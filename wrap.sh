24b1e9f87688c06bd23eb07efe265c6717ee1751
Michael Gelbart <mgelbart@seas.harvard.edu>
2012-08-03 15:16:04 -0400

    update wrap to deal with default DATA_INFO file more gracefully

--- wrap.sh
+++ wrap.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+mv DATA_INFO.csv DATA_INFO_TEMP.csv
+mv DATA_INFO_DEFAULT.csv DATA_INFO.csv
 zip -r EDGE-$1.zip DATA_INFO.csv install.m java1 Matlab Measurements
+mv DATA_INFO.csv DATA_INFO_DEFAULT.csv
+mv DATA_INFO_TEMP.csv DATA_INFO.csv
